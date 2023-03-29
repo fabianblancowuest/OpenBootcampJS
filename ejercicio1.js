60d5240ddfe0cc6b17c2b2e5c072c64fb1533eb3
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2023-03-29 05:12:30 -0300

    ejercicio 2 - creando variables tipo strings, numbers, fechas y objetos

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+let nombre = "Fabián";
+let edad = 32;
+let eresDesarrollador = true;
+let fechaNacimiento = new Date(1990, 05, 29);
+
+const libroFavorito = {
+	titulo: "You don't know JS",
+	autor: "Kyle Simpson",
+	fecha: new Date(2020, 0, 28),
+	url: "https://github.com/getify/You-Dont-Know-JS",
+};
+
+console.log(libroFavorito.fecha);
