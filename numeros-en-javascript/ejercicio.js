f048e3d78fb2f26137e7913310b1db4319d84a85
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2023-03-31 09:01:26 -0300

    ejercicio: números en JS

---
+++ numeros-en-javascript/ejercicio.js
@@ -0,0 +1,9 @@
+const alturaCM = 177;
+const alturaMT = 1.77;
+
+let peso = 97.5;
+const alturaRedondeada = Math.round(alturaMT);
+
+let pesoRedondeado = Math.floor(peso);
+
+let maxVal = Number.MIN_VALUE + 1 === Number.MIN_VALUE;
